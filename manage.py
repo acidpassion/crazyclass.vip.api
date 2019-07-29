be66bf73e5011b6543fdf610a64db404f4ce335e
Alex Xie <alex.xie.micro@outlook.com>
2019-07-29 22:34:24 +0800

    updated

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
-    app.run()
+    app.run(host='0.0.0.0', ssl_context=('/root/ssl/2498249_crazyclass.vip.pem', '/root/ssl/2498249_crazyclass.vip.key'))
 
 
 @manager.command
